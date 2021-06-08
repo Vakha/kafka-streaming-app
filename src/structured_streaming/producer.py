163ea1eb7cb6ab6fa8716f32322b47dfb491f871
Alex Vakhmenin <alex@vakhmenin.com>
2021-06-08 17:32:48 +0200

    Init project

---
+++ src/structured_streaming/producer.py
@@ -0,0 +1,14 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:29092'],
+    key_serializer=lambda x: str(x).encode('utf-8'),
+    value_serializer=lambda x: dumps(x).encode('utf-8')
+)
+
+for e in range(100):
+    data = {'number' : e}
+    producer.send('output-topic', key=e, value=data)
+    sleep(1)
---
+++ src/structured_streaming/word_count.py
@@ -0,0 +1,36 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+spark = SparkSession \
+    .builder \
+    .appName("StructuredNetworkWordCount") \
+    .getOrCreate()
+
+# Create DataFrame representing the stream of input lines from connection to localhost:9999
+lines = spark \
+    .readStream \
+    .format("socket") \
+    .option("host", "localhost") \
+    .option("port", 9999) \
+    .load()
+
+# Split the lines into words
+words = lines.select(
+   F.explode(
+       F.split(lines.value, " ")
+   ).alias("word")
+)
+
+# Generate running word count
+wordCounts = words.groupBy("word").count()
+
+ # Start running the query that prints the running counts to the console
+query = wordCounts \
+    .writeStream \
+    .outputMode("complete") \
+    .format("console") \
+    .start()
+
+print("run `nc -lk 9999` in the different terminal and send words")
+
+query.awaitTermination()
